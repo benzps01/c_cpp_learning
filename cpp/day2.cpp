99c852ac6dc7f8acfce52955bc1ea26642161284
benzps01 <benzps01@gmail.com>
2024-03-18 23:52:23 +0530

    testing cin and cout

--- cpp/day2.cpp
+++ cpp/day2.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <string>
 
 int main()
 {
+    /*
     std::cout << "Hello C++20 " << std::endl;
 
     int age{21};
@@ -9,5 +11,16 @@ int main()
 
     std::cerr << "Error Message: something is wrong!" << std::endl;
     std::clog << "Log message : Something happened " << std::endl;
+    */
+
+    int age1;
+    std::string name;
+
+    std::cout << "Please type your name and age: " << std::endl;
+
+    std::cin >> name >> age1;
+    // std::cin >> age1;
+
+    std::cout << "Hello " << name << " you are " << age1 << " years old! " << std::endl;
     return 0;
 }
