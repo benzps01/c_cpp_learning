765bc746ce43ab8e7fcbcdb4a9d21f25b41bd2a9
benzps01 <benzps01@gmail.com>
2024-03-19 22:44:22 +0530

    add data with space in them

--- cpp/day2.cpp
+++ cpp/day2.cpp
@@ -13,14 +13,31 @@ int main()
     std::clog << "Log message : Something happened " << std::endl;
     */
 
-    int age1;
-    std::string name;
+    // Data input
+    /*
+     int age1;
+     std::string name;
 
-    std::cout << "Please type your name and age: " << std::endl;
+     std::cout << "Please type your name and age: " << std::endl;
 
-    std::cin >> name >> age1;
-    // std::cin >> age1;
+     std::cin >> name >> age1;
+     // std::cin >> age1;
 
-    std::cout << "Hello " << name << " you are " << age1 << " years old! " << std::endl;
+     std::cout << "Hello " << name << " you are " << age1 << " years old! " << std::endl;
+     return 0;
+     */
+
+    // Data with spaces
+    std::string full_name;
+    int age3;
+
+    std::cout << "Please type in your full name and age " << std::endl;
+
+    std::getline(std::cin, full_name); // gets data from the line even if it has spaces....
+
+    std::cin >> age3;
+
+    std::cout << "Hello " << full_name << " you are " << age3 << " years old! " << std::endl;
+
     return 0;
 }
